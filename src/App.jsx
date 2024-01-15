cfbf9f609849233a75b8a7fd79e362e2a00a1ebe
ALPINISTLAR <niveam424@gmail.com>
2024-01-15 22:07:58 +0200

    remove unnecessary items

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,7 @@ function App() {
 
   return (
     <>
-      <div className='container'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. At id necessitatibus porro soluta accusamus quam ipsa numquam doloremque, natus itaque, maiores voluptate reiciendis ad dignissimos illum consectetur quis, placeat accusantium.
-        Dolores eos iste libero earum cupiditate aliquam eveniet nobis excepturi nam dolorum ducimus nisi enim consequuntur officiis molestias nesciunt vitae voluptatem a veritatis laborum, amet deleniti magni quaerat autem. Amet.
-        Ipsa consectetur blanditiis minima repellat molestias autem commodi quidem in, nesciunt dolor sequi harum tempora exercitationem eveniet similique nulla. Delectus hic ipsa beatae cumque quos nam neque. Voluptatum, dignissimos iste.
-        Libero, mollitia obcaecati commodi facere ex nemo sunt ullam quae minus repellat harum earum! Id esse debitis nemo repudiandae illo omnis pariatur itaque, autem voluptatibus vero, placeat aliquam, voluptates sint!
-        Tempora consectetur ad voluptate optio omnis rem dolorem praesentium laudantium. Autem perspiciatis eius in! Magni ratione quasi voluptatibus nesciunt ex laudantium molestias, laborum, eveniet quaerat laboriosam illum quos optio praesentium?
-        Sit, quasi. A quibusdam corporis nulla sit ex ea laborum id nam minus, iste repellendus omnis, vel, repellat nobis. Provident minima facere eligendi incidunt, tempore quibusdam pariatur odit voluptatem temporibus?
-        Quisquam laboriosam accusantium magnam nihil! Hic veniam iste libero exercitationem dolores aliquam ducimus id cupiditate, explicabo sapiente omnis dolor recusandae fugiat in magnam consequuntur blanditiis et ea natus, suscipit voluptatum.
-        Quae dolor quibusdam libero quia, ducimus reprehenderit eaque illo et nulla, quos voluptas, excepturi adipisci vitae. Eligendi optio maxime fugiat, in, labore expedita ea nemo distinctio laboriosam unde doloribus exercitationem.
-        Accusantium quas tempore rerum! Delectus facere minus ad maiores fuga obcaecati alias dolores perferendis quo pariatur. Veniam tenetur rem, dicta natus illum laboriosam officiis, cumque animi nemo fugiat reiciendis eveniet.
-        Recusandae sed ut, dolorem nisi sequi, commodi inventore adipisci tempora fugit, accusantium dolor. Neque nam fugit quis reiciendis totam qui assumenda, incidunt blanditiis natus similique expedita eum, rerum beatae dignissimos?
-        Rem aliquam mollitia consectetur porro saepe? Sit aliquid qui totam cumque voluptates fugiat, asperiores exercitationem unde ipsum quae laudantium earum nisi maxime perferendis amet ea nobis consectetur aspernatur voluptate iure.
-        Id, aspernatur ducimus rerum ut laborum fugiat ad eaque ipsum error earum enim amet, doloribus quae aut odit placeat nostrum, officia suscipit nihil consequatur. Enim officia saepe error id odio.
-        Vero fugiat non adipisci voluptas facere consequuntur a minus numquam obcaecati maxime molestias soluta, quaerat pariatur consequatur at commodi dicta ad veniam quibusdam tempore ducimus reiciendis hic. Rerum, doloribus esse.
-        Neque possimus fugit iusto sunt iure, incidunt eius ipsa qui nesciunt distinctio alias adipisci nemo commodi perferendis id vitae. Debitis hic dolor neque iste illum rem harum quidem enim exercitationem?
-        Numquam, magni quidem natus culpa vel aut pariatur id nostrum libero! Voluptatibus ducimus modi molestiae, minus, odio et deleniti consequatur impedit nam perferendis magni praesentium magnam? Enim quae eius animi.
-        Dolorum ipsa pariatur doloribus architecto, adipisci consequuntur sit ducimus consequatur commodi excepturi, ab recusandae quibusdam maiores eum molestiae amet eius tenetur quaerat velit, natus ullam! Ea assumenda odit at est.
-        Fugiat harum reprehenderit cupiditate commodi earum sapiente asperiores in aliquid, nemo eos inventore velit eius fuga? Provident iusto quos reiciendis quae placeat, aperiam mollitia accusantium vitae? Expedita quaerat alias deserunt.
-        Ab, totam! Dolores at impedit, sit non delectus officia nisi minus necessitatibus dolore iusto saepe illum excepturi. Amet quasi quaerat modi quis, cupiditate reprehenderit molestiae aut, corrupti nostrum illo magnam!
-        Expedita amet sit in harum similique ipsum, accusantium excepturi quae asperiores possimus mollitia aut culpa sed sint optio animi quos quo iusto unde nesciunt ad quis ipsa? A, exercitationem voluptate!
-        Nihil repellendus nam, molestiae sint suscipit aut, earum ex doloribus eaque accusamus dicta veritatis minus! Reiciendis minima eos natus facere officiis soluta a ratione, molestiae, nemo sit rerum aliquam esse.
-      </div>
+
     </>
   )
 }
