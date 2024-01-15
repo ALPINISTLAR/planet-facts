73b352e59b3ebe94e7de0f1b1269fbc5b5e9664e
ALPINISTLAR <niveam424@gmail.com>
2024-01-15 16:21:53 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laborum magnam blanditiis nulla sit recusandae ratione incidunt, debitis suscipit adipisci reprehenderit eveniet nobis illo earum beatae deserunt sequi maiores id.
+      Dolor, mollitia. Facilis at, provident aperiam velit assumenda iusto facere, corporis officiis ad illum omnis consequuntur commodi alias voluptatibus aliquam laboriosam consequatur suscipit ea iure, debitis sit. Quam, suscipit repellat!
+      Veritatis commodi beatae, ad neque suscipit minima quaerat, consequatur architecto maiores soluta vitae molestias iure minus repellendus nemo velit accusamus! Obcaecati, corporis. Ducimus officia nesciunt minus unde sapiente repudiandae blanditiis!
+      Sint maxime libero nam expedita tempore, fugiat ipsum optio sapiente eligendi! Minima possimus, suscipit excepturi quae labore nam. Amet dolores laudantium consequatur dignissimos odio culpa perspiciatis possimus, voluptatibus mollitia commodi.
+      Iure libero nulla facilis ab maiores nihil, vel distinctio impedit quas eveniet, enim aperiam tempora expedita obcaecati neque amet officiis eius quibusdam repudiandae earum. Voluptas dignissimos fugiat molestiae numquam quasi.
+      Quae, laborum laudantium amet nostrum, recusandae porro iste odit voluptate sequi exercitationem enim saepe id repellendus accusantium laboriosam tempora nam nesciunt est adipisci quia at modi. Ipsam reiciendis ea officiis.
+      Quis exercitationem harum temporibus aliquid quidem ipsum perspiciatis illo esse dolore doloribus nam modi corrupti asperiores neque saepe, ad fugiat porro ducimus iusto. Ea quaerat totam a consectetur esse quia!
+      Laudantium labore soluta commodi totam, expedita cupiditate. Aperiam alias earum veniam rerum ea commodi, incidunt eum dolor ab unde qui voluptatibus porro perferendis totam, pariatur facere molestias? Iste, dicta quisquam!
+      Corporis nisi voluptas soluta, explicabo veritatis molestias ad incidunt quaerat architecto tempora nulla eligendi quia culpa! Officiis mollitia provident architecto itaque sapiente, inventore esse fugiat quas? Ipsum eos voluptatem maxime.
+      Labore consequuntur ipsa ex provident quibusdam, in modi illum officiis optio vel libero esse similique eius non cumque illo alias beatae autem perspiciatis consectetur, odit repellendus. Animi, accusantium. Quidem, sit?
     </>
   )
 }
