79cb6a3b76a77c8af02c2cc3c1b7e4c6332b5e4b
ALPINISTLAR <niveam424@gmail.com>
2024-01-16 23:17:51 +0200

    add

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -8,6 +8,7 @@ import Heading from '../components/headings/Heading';
 import PlanetContent from '../components/planetContent/PlanetContent';
 import PlanetContentContainer from '../components/planetContent/planetContentContainer/PlanetContentContainer';
 import ContentNavigationContainer from '../components/planetContentNavigation/ContentNavigationContainer';
+import PlanetContentContainer from '../components/planetContent/planetContentContainer/PlanetContentContainer';
 import OverviewBtn from '../components/planetContentNavigation/OverviewBtn';
 import InternalStructureBtn from '../components/planetContentNavigation/InternalStructureBtn';
 import SurfaceGeologyBtn from '../components/planetContentNavigation/SurfaceGeologyBtn';
