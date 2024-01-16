4476826d66cb882777f9eb6f107bc7c86d0da612
ALPINISTLAR <niveam424@gmail.com>
2024-01-16 23:13:59 +0200

    add

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -6,7 +6,7 @@ import PlanetContainer from '../components/planets/PlanetContainer';
 import MainSection from '../components/main/MainSection';
 import Heading from '../components/headings/Heading';
 import PlanetContent from '../components/planetContent/PlanetContent';
-
+import PlanetContentContainer from '../components/planetContent/planetContentContainer/PlanetContentContainer';
 import ContentNavigationContainer from '../components/planetContentNavigation/ContentNavigationContainer';
 import OverviewBtn from '../components/planetContentNavigation/OverviewBtn';
 import InternalStructureBtn from '../components/planetContentNavigation/InternalStructureBtn';
