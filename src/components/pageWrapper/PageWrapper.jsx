c17b2686bdbfde3b12fbe473b7d52c1201c51e41
ALPINISTLAR <niveam424@gmail.com>
2024-01-16 21:48:41 +0200

    page wrapper

---
+++ src/components/pageWrapper/PageWrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: ${({ theme }) => theme.screenSize.desktop};
+  position: relative;
+`;
+
+const PageWrapper = ({ children, className }) => {
+  return <Wrapper className={className}>{children}</Wrapper>;
+};
+
+export default PageWrapper;
