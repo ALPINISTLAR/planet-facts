756f6fa28bc56519e5e9c7aa3e59d46f86d27c18
ALPINISTLAR <niveam424@gmail.com>
2024-01-16 21:53:01 +0200

    change reactRoot.render to ReactDOM.createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
+const root = document.getElementById('root');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+// ReactDOM.render ni createRoot bilan almashtirish
+const reactRoot = createRoot(root);
+
+// React.StrictMode ni qo'shish
+reactRoot.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
